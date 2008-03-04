ff92ecc1548906a34b5123c2c005287787f0dfee
Phil Hagelberg <phil@hagelb.org>
2008-03-04 10:00:13 -0800

    don't run tests if you can't load the files they test

--- test/test_web.rb
+++ test/test_web.rb
@@ -9,14 +9,15 @@ module BusScheme
   end
 end
 
-class WebTest < Test::Unit::TestCase
-  def setup
-    @response = nil
+if defined? BusScheme::Resource
+  class WebTest < Test::Unit::TestCase
+    def setup
+      @response = nil
 
-    @die_roboter = "User-agent: *\nAllow: *"
-    eval "(resource \"/robots.txt\" \"#{@die_roboter}\")"
-    
-    eval '(define concourse-splash (quote (html
+      @die_roboter = "User-agent: *\nAllow: *"
+      eval "(resource \"/robots.txt\" \"#{@die_roboter}\")"
+      
+      eval '(define concourse-splash (quote (html
 		(head
 		 (title "Concourse"))
 		(body
@@ -27,46 +28,47 @@ class WebTest < Test::Unit::TestCase
 			    (input type "text" name "email")
 			    (input type "password" name "password")
 			    (input type "submit" value "Log in")))))))'
-    eval '(resource "/" concourse-splash)'
-  end
-  
-  def test_serves_string_resource
-    get '/robots.txt'
-    assert_response_code 200
-    assert_response @die_roboter
-  end
+      eval '(resource "/" concourse-splash)'
+    end
+    
+    def test_serves_string_resource
+      get '/robots.txt'
+      assert_response_code 200
+      assert_response @die_roboter
+    end
 
-  def test_serves_list_resource
-    get '/'
-    assert_response_code 200
-    # TODO: make this a little less picky.
-    assert_response "<html>\n  <head>\n    <title>\nConcourse    </title>\n  </head>\n  <body>\n    <div id=\"container\">\n      <h1>\nWelcome to Concourse!      </h1>\n      <form action=\"/login\">\n        <input type=\"text\" name=\"email\">\n        </input>\n        <input type=\"password\" name=\"password\">\n        </input>\n        <input type=\"submit\" value=\"Log in\">\n        </input>\n      </form>\n    </div>\n  </body>\n</html>\n"
-  end
+    def test_serves_list_resource
+      get '/'
+      assert_response_code 200
+      # TODO: make this a little less picky.
+      assert_response "<html>\n  <head>\n    <title>\nConcourse    </title>\n  </head>\n  <body>\n    <div id=\"container\">\n      <h1>\nWelcome to Concourse!      </h1>\n      <form action=\"/login\">\n        <input type=\"text\" name=\"email\">\n        </input>\n        <input type=\"password\" name=\"password\">\n        </input>\n        <input type=\"submit\" value=\"Log in\">\n        </input>\n      </form>\n    </div>\n  </body>\n</html>\n"
+    end
 
-  def test_serves_404
-    get '/404'
-    assert_response_code 404
-    assert_response_match /not found/i
-  end
-  
-  private
-  
-  def get path
-    @response = Rack::MockRequest.new(BusScheme.web_server).get(path)
-  end
-  
-  def assert_response expected, message = nil
-    raise "No request has been made!" if @response.nil?
-    assert_equal expected, @response.body, message
-  end
+    def test_serves_404
+      get '/404'
+      assert_response_code 404
+      assert_response_match /not found/i
+    end
+    
+    private
+    
+    def get path
+      @response = Rack::MockRequest.new(BusScheme.web_server).get(path)
+    end
+    
+    def assert_response expected, message = nil
+      raise "No request has been made!" if @response.nil?
+      assert_equal expected, @response.body, message
+    end
 
-  def assert_response_match expected, message = nil
-    raise "No request has been made!" if @response.nil?
-    assert_match expected, @response.body, message
-  end
+    def assert_response_match expected, message = nil
+      raise "No request has been made!" if @response.nil?
+      assert_match expected, @response.body, message
+    end
 
-  def assert_response_code expected, message = nil
-    raise "No request has been made!" if @response.nil?
-    assert_equal expected, @response.status, message
+    def assert_response_code expected, message = nil
+      raise "No request has been made!" if @response.nil?
+      assert_equal expected, @response.status, message
+    end
   end
 end
--- test/test_xml.rb
+++ test/test_xml.rb
@@ -1,32 +1,34 @@
 $LOAD_PATH << File.dirname(__FILE__)
 require 'test_helper'
 require 'open-uri'
-require 'hpricot'
 
-class XmlTest < Test::Unit::TestCase
-  def test_single_cons
-    assert_equal_xml "<html> </html>", eval("(xml (html))")
-  end
-  
-  def test_singly_nested_list
-    assert_equal_xml("<html> <title> </title> </html>",
-                     eval("(xml (html (title)))"))
-  end
+begin
+  require 'hpricot'
 
-  def test_list_with_string
-    assert_equal_xml("<html> <title> Hello </title> </html>",
-                     eval("(xml (html (title \"Hello\")))"))
+  class XmlTest < Test::Unit::TestCase
+    def test_single_cons
+      assert_equal_xml "<html> </html>", eval("(xml (html))")
+    end
+    
+    def test_singly_nested_list
+      assert_equal_xml("<html> <title> </title> </html>",
+                       eval("(xml (html (title)))"))
+    end
 
-  end
+    def test_list_with_string
+      assert_equal_xml("<html> <title> Hello </title> </html>",
+                       eval("(xml (html (title \"Hello\")))"))
 
-  def test_list_with_symbol
-    assert_equal_xml("<a href=\"http://bus-scheme.rubyforge.org\"> Bus Scheme</a>",
-                     eval("(xml (a href \"http://bus-scheme.rubyforge.org\" \"Bus Scheme\"))"))
-  end
+    end
 
-  # TODO: no idea why this puts {} on stdout
-  def test_splash_page_generation
-    sexp = '(html
+    def test_list_with_symbol
+      assert_equal_xml("<a href=\"http://bus-scheme.rubyforge.org\"> Bus Scheme</a>",
+                       eval("(xml (a href \"http://bus-scheme.rubyforge.org\" \"Bus Scheme\"))"))
+    end
+
+    # TODO: no idea why this puts {} on stdout
+    def test_splash_page_generation
+      sexp = '(html
 		(head
 		 (title "Concourse"))
 		(body
@@ -37,7 +39,7 @@ class XmlTest < Test::Unit::TestCase
 			    (input type "text" name "email")
 			    (input type "password" name "password")
 			    (input type "submit" value "Log in")))))'
-    xml_text = "<html> <head> <title> Concourse </title> </head>
+      xml_text = "<html> <head> <title> Concourse </title> </head>
 <body> <div id=\"container\"> <h1> Welcome to Concourse! </h1>
 <form action=\"/login\">
   <input name=\"email\" type=\"text\" />
@@ -45,12 +47,16 @@ class XmlTest < Test::Unit::TestCase
   <input type=\"submit\" value=\"Log in\" />
 </form>
 </div> </body> </html>"
-    assert_equal_xml xml_text, eval("(xml #{sexp})")
-   end
+      assert_equal_xml xml_text, eval("(xml #{sexp})")
+    end
 
-  private
-  def assert_equal_xml(expected, actual, message = nil)
-    # TODO: whitespace handling could be better
-    assert_equal Hpricot(expected).to_s.gsub(/\s+/, ' ').strip, Hpricot(actual).to_s.gsub(/\s+/, ' ').strip, message
+    private
+    def assert_equal_xml(expected, actual, message = nil)
+      # TODO: whitespace handling could be better
+      assert_equal Hpricot(expected).to_s.gsub(/\s+/, ' ').strip, Hpricot(actual).to_s.gsub(/\s+/, ' ').strip, message
+    end
   end
+
+rescue LoadError
+  puts "Can't test XML generation without Hpricot; sorry."
 end
