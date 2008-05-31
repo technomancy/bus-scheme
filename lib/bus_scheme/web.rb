b4dc6f32a04ee8a0d445b2cd227dd255bdcc0b1b
Jack Danger Canty <git@6brand.com>
2008-05-31 16:22:21 -0700

    webwait allows a web server to not immediately exit

--- lib/bus_scheme/web.rb
+++ lib/bus_scheme/web.rb
@@ -11,8 +11,12 @@ module BusScheme
   module Web
     def self.serve(port = 2000)
       # TODO: fallback to webrick if mongrel is not found
-      @web_server ||= lambda { |env| Resource[env].call(env) }
-      @web_thread ||= Thread.new { Rack::Handler::Mongrel.run @web_server, :Port => port }
+      @server ||= lambda { |env| Resource[env].call(env) }
+      @thread ||= Thread.new { Rack::Handler::Mongrel.run @server, :Port => port }
+    end
+    
+    def self.thread
+      @thread
     end
 
     def self.redirect(to, headers = {})
@@ -20,4 +24,7 @@ module BusScheme
       [302, headers.merge({'location' => to}), '']
     end
   end
+  
+  define 'webwait', primitive { Thread.join(Web.thread) }
+  
 end
