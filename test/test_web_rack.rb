4409ebb559f50844795e29dce4d62bb8b57d1d64
Jack Danger Canty <git@6brand.com>
2008-06-01 20:40:52 -0700

    Initial work done on full-scheme rack apps.
    Lots of work left to be done but you can now specify
    a rack app in scheme.  It's a lambda that is given the
    env and returns a cons that represents the status,
    headers, and body of the response.

---
+++ test/test_web_rack.rb
@@ -0,0 +1,73 @@
+$LOAD_PATH << File.dirname(__FILE__)
+require 'test_helper'
+require 'open-uri'
+
+if defined? BusScheme::Web::Resource
+  module BusScheme
+    module_function
+    def Web.server # need to expose this for MockRequest
+      @server
+    end
+  end
+
+  class WebRackTest < Test::Unit::TestCase
+    def setup
+      @response = nil
+      app = '(lambda (env) (quote ("200" ("Content-Type" "text/plain") "This is Simple")))'
+      @simple_app = eval!(app) 
+      eval! "(defwebapp \"/simple\" #{app})"
+    end
+    
+    def test_app_can_be_called
+      @simple_app.call({'PATH_INFO' => '/simple'})
+    end
+    
+    def test_returns_status_code
+      get '/simple'
+      assert_response_code 200
+    end
+
+    def test_returns_content_type
+      get '/simple'
+      assert_equal 'text/plain', @response.headers['Content-Type']
+    end
+
+    private
+    
+    def get(path)
+      @response = mock_request.get(path)
+    end
+
+    def put(path, contents)
+      @response = mock_request.put(path, { :input => contents })
+    end
+
+    def post(path, contents)
+      @response = mock_request.post(path, contents)
+    end
+    
+    def delete(path)
+      @response = mock_request.delete(path)
+    end
+        
+    def mock_request
+      Rack::MockRequest.new(BusScheme::Web.server)
+    end
+    
+    def assert_response expected, message = nil
+      raise "No request has been made!" if @response.nil?
+      # TODO: make this a little less picky about whitespace etc.
+      assert_equal expected, @response.body, message
+    end
+
+    def assert_response_match expected, message = nil
+      raise "No request has been made!" if @response.nil?
+      assert_match expected, @response.body, message
+    end
+
+    def assert_response_code expected, message = nil
+      raise "No request has been made!" if @response.nil?
+      assert_equal expected, @response.status, message
+    end
+  end
+end
