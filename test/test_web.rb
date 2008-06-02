be25ef0d6138f5f93c16980f0032a502688e0520
Jack Danger Canty <git@6brand.com>
2008-06-01 22:00:37 -0700

    Folding the scheme rack app tests into test_web.rb

--- test/test_web.rb
+++ test/test_web.rb
@@ -31,6 +31,18 @@ if defined? BusScheme::Web::Resource
       eval! '(defresource "/" concourse-splash)'
 
       eval! '(defresource "/time" (lambda (env) (send (now) (quote to_s))))'
+
+      # a very basic rack app in scheme registered at "/simple"
+      simple_lambda = '(lambda (env) (quote ("200" ("Content-Type" "text/plain") "This is Simple")))'
+      @simple_app = eval!(simple_lambda)
+      eval! "(defwebapp \"/simple\" #{simple_lambda})"
+      
+      # this app returns the SERVER_INFO from the env passed to it
+      # TODO: come up with a better method for easily returning these values
+      eval! '(defwebapp "/who-am-i" (lambda (env)
+                                            (cons "200"
+                                                  (cons (quote ("Content-Type" "text/html"))
+                                                        (env "PATH_INFO")))))'
     end
     
     def test_serves_string_resource
@@ -115,6 +127,32 @@ Concourse is ...      </p>
       assert_response_code 404
     end
 
+    def test_app_can_be_called
+      @simple_app.call({'PATH_INFO' => '/simple'})
+    end
+    
+    def test_returns_status_code
+      get '/simple'
+      assert_response_code 200
+    end
+
+    def test_returns_content_type
+      get '/simple'
+      assert_equal 'text/plain', @response.headers['Content-Type']
+    end
+    
+    def test_returns_body
+      get '/simple'
+      assert_equal 'This is Simple', @response.body
+    end
+
+    def test_returning_environment_as_body
+      get '/who-am-i'
+      assert_equal "/who-am-i", @response.body
+      assert_equal 'text/html', @response.headers['Content-Type']
+      assert_response_code 200
+    end
+
 #     def test_returns_forbidden_when_unauthorized
 #     end
 
@@ -125,6 +163,7 @@ Concourse is ...      </p>
 
 (web-get \"http://localhost:2000/\")"
     end
+
     private
     
     def get(path)
